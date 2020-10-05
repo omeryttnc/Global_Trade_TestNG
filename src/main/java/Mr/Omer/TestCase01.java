6e676f571c7c991433650123991f7b27d2b87aa8
omeryttnc <omeryttnc@gmail.com>
2020-10-05 22:11:35 +0300

    first commit

---
+++ src/main/java/Mr/Omer/TestCase01.java
@@ -0,0 +1,11 @@
+package Mr.Omer;
+
+import org.testng.annotations.Test;
+
+public class TestCase01 {
+    @Test
+    public void helloWorld(){
+        System.out.println("hello World");
+    }
+
+}
