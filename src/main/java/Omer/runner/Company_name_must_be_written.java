483d56958c969da43a9ece938c420110920d1331
omeryttnc <omeryttnc@gmail.com>
2020-10-11 02:20:50 +0300

    Positive_scenario ready but drop down menus

---
+++ src/main/java/Omer/runner/Company_name_must_be_written.java
@@ -0,0 +1,11 @@
+package Omer.runner;
+
+import org.testng.annotations.Test;
+
+public class Company_name_must_be_written {
+    @Test
+    public void Company_name() throws InterruptedException {
+        Seller_Join_Negative.Go_to_agent_seller();
+
+    }
+}
